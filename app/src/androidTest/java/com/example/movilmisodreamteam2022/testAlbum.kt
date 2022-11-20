0594f4503056d4a728217efe92a98a9ff1447414
Andres Cardenas <a.cardenasj@uniandes.edu.co>
2022-11-20 15:27:29 -0500

    actu

--- app/src/androidTest/java/com/example/movilmisodreamteam2022/testAlbum.kt
+++ app/src/androidTest/java/com/example/movilmisodreamteam2022/testAlbum.kt
@@ -32,18 +32,12 @@ class testAlbum {
         var nombreAlbun = "AndresAlbum"
         var albumYear = "1991"
         var nombreArtista = "Andres Cardenas"
-        var cancion  = "animals"
-        var genero  = "Salsa"
         var des = "Descripcion"
-        var sello = "EMI"
 
         onView(withId(R.id.EDNombreAlbum)).perform(ViewActions.typeText(nombreAlbun.toString()))
         onView(withId(R.id.EDYearAlbum)).perform(ViewActions.typeText(albumYear.toString()))
         onView(withId(R.id.EDArtista)).perform(ViewActions.typeText(nombreArtista.toString()))
-        onView(withId(R.id.EDCancionPreferida)).perform(ViewActions.typeText(cancion.toString()))
-        onView(withId(R.id.EDGenero)).perform(ViewActions.typeText(genero.toString()))
         onView(withId(R.id.EDDescrip)).perform(ViewActions.typeText(des.toString()))
-        onView(withId(R.id.EDRLabel)).perform(ViewActions.typeText(sello.toString()))
         Espresso.closeSoftKeyboard()
         onView(withId(R.id.BtnCrearAlbum)).perform(click())
         onView(withId(R.id.lblRequestCrearAlbum))
