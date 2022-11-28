92b64606d0d11742231228a17faebc116ce87acc
JUAN SEBASTIAN SANCHEZ L <js.sanchezl12@uniandes.edu.co>
2022-11-28 12:30:24 -0500

    flujo_test de crear album
    Flujo completo desde que entra a actividad principal

--- app/src/androidTest/java/com/example/movilmisodreamteam2022/testCrearAlbum.kt
+++ app/src/androidTest/java/com/example/movilmisodreamteam2022/testCrearAlbum.kt
@@ -12,6 +12,7 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
 import com.example.movilmisodreamteam2022.ui.CrearAlbumActivity
+import com.example.movilmisodreamteam2022.ui.MainActivity
 import org.hamcrest.Matchers.containsString
 import org.junit.Rule
 import org.junit.Test
@@ -22,20 +23,25 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class testCrearAlbum {
 
-
     @get:Rule
-    val activityRuleCrearAlbum = ActivityTestRule(CrearAlbumActivity::class.java)
+    val activityRuleMain = ActivityTestRule(MainActivity::class.java)
 
+    //@get:Rule
+    //val activityRuleCrearAlbum = ActivityTestRule(CrearAlbumActivity::class.java)
 
     @Test
     fun testCreateAlbum() {
 
-        var nombreAlbun = "AndresAlbum"
+        var nombreAlbun = "Andres Album"
         var albumYear = "1991"
         var des = "Descripcion"
         var selectionText = "Classical"
         var Sello = "Sony Music"
-
+        Thread.sleep(3000)
+        onView(withId(R.id.button_album_m)).perform(click())
+        Thread.sleep(3000)
+        onView(withId(R.id.fab_add_album)).perform(click())
+        Thread.sleep(3000)
         onView(withId(R.id.EDNombreAlbum)).perform(ViewActions.typeText(nombreAlbun.toString()))
         onView(withId(R.id.EDYearAlbum)).perform(ViewActions.typeText(albumYear.toString()))
         onView(withId(R.id.spinnerGenero)).check(matches(withSpinnerText(containsString(selectionText))))
@@ -43,8 +49,9 @@ class testCrearAlbum {
         onView(withId(R.id.spinnerSello)).check(matches(withSpinnerText(containsString(Sello))))
         Espresso.closeSoftKeyboard()
         onView(withId(R.id.BtnCrearAlbum)).perform(click())
-        onView(withId(R.id.lblRequestCrearAlbum))
-            .check(matches(withText("Album creado correctamente")))
+        Thread.sleep(500)
+        onView(withId(R.id.lblRequestCrearAlbum)).check(matches(withText("Album creado correctamente")))
+        Thread.sleep(5000)
     }
 
 
